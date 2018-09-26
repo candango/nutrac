4b49170be2a00fc45e02ff196146e495d4f95ee6
Flavio Garcia <piraz@candango.org>
2018-09-25 21:40:15 -0400

    Added handlers and set the port to run at 9999.

--- intrac/handlers.py
+++ intrac/handlers.py
@@ -1,7 +1,19 @@
-import firenado.tornadoweb
+from firenado import tornadoweb
 
 
-class IndexHandler(firenado.tornadoweb.TornadoHandler):
+class IndexHandler(tornadoweb.TornadoHandler):
 
     def get(self):
         self.write("IndexHandler output")
+
+
+class ProfileHandler(tornadoweb.TornadoHandler):
+
+    def get(self):
+        self.write("Profile output")
+
+
+class ProjectsHandler(tornadoweb.TornadoHandler):
+
+    def get(self, user):
+        self.write("Projects output %s" % user)
