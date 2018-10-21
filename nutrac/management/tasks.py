845c868d08d8909ba274af9971ac67f66db1f737
Flavio Garcia <piraz@candango.org>
2018-10-21 03:35:12 -0400

    Introduced database arguments to the create db task.
    Refs: #6

--- nutrac/management/tasks.py
+++ nutrac/management/tasks.py
@@ -1,17 +1,31 @@
 from firenado.management import ManagementTask
-from firenado.util import file as _file
+from firenado.util import file as _file, sqlalchemy_util
 import logging
 import os
-from sqlalchemy import text
+from six import moves
 
 logger = logging.getLogger(__name__)
 
 
 class NutacCreateDatabaseTask(ManagementTask):
-    """ Create the database
-    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("-d", "--database", default="nutrac")
+        parser.add_argument("-H", "--host", default="localhost")
+        parser.add_argument("-p", "--password")
+        parser.add_argument("-P", "--port", default=5432, type=int)
+        parser.add_argument("-u", "--user", default="nutrac")
+
     def run(self, namespace):
+        """ Create the database
+        """
+        password = None
+        if namespace.password is None:
+            password = moves.input("Inform the database password:")
+        else:
+            password = namespace.password
         print(namespace)
+        exit(2)
 
         db_script_path = os.path.realpath(
             os.path.join(
@@ -24,6 +38,6 @@ class NutacCreateDatabaseTask(ManagementTask):
         )
         public_script_path = _file.read(db_script_path).replace(
             "<INTRAC_USER>", "intracuser")
-        print(public_script_path)
-        logger.info("Creating the NuTrac database.")
+        sqlalchemy_util.run_script(public_script_path)
 
+        logger.info("Creating the NuTrac database.")
