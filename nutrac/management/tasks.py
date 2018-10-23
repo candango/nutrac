b2ea70211f00464edc295d2f20456d416aae6920
Flavio Garcia <piraz@candango.org>
2018-10-22 20:32:23 -0400

    Started to add the admin user to a new instalation.
    Refs: #6

--- nutrac/management/tasks.py
+++ nutrac/management/tasks.py
@@ -1,10 +1,29 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ..models import UserBase
+from ..security import generate_private_key
 from firenado.management import ManagementTask
-from firenado.util import file as _file
 from firenado.util.sqlalchemy_util import Session, run_script
 import logging
 import os
 from six import moves
 from sqlalchemy import create_engine
+import uuid
+
 
 logger = logging.getLogger(__name__)
 
@@ -59,5 +78,10 @@ class NutacCreateDatabaseTask(ManagementTask):
 
         run_script(db_script_path, session, handle_command=fix_command)
 
+        user = UserBase()
+        user.uuid = str(uuid.uuid5(uuid.NAMESPACE_URL, "http://localhost"))
+        user.username = "nutracmin"
+        user.password = "nutracpass"
+        user.private_key = generate_private_key()
 
         logger.info("Creating the NuTrac database.")
---
+++ nutrac/security.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO: Private key methods belong to podship_platform project
+def generate_private_key():
+    """ FROM pyraspora: pyaspora.user.models
+    Generate a 4096-bit RSA key. The key will be stored in the User
+    object. The private key will be protected with password <passphrase>,
+    which is usually the user password.
+    """
+    # TODO: seems to be candidate as part of some security toolkit
+    from Crypto.PublicKey import RSA
+    RSAkey = RSA.generate(4096)
+    return RSAkey.exportKey(
+        format='PEM',
+        pkcs=1
+    ).decode("ascii")
+
