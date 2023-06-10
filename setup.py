c1d0e47afbd8779f84c964e082422c785fbe2a7e
Flavio Garcia <piraz@candango.org>
2023-06-10 00:46:50 -0400

    refactor(build): add project info to the nutrac module
    Use project info in the setup function.
    
    Refs: #1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2018 Flavio Garcia
+# Copyright 2018-2023 Flavio Garcia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,27 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import nutrac
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+import sys
 
-import os
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    print("error: Upgrade to a pip version newer than 10. Run \"pip install "
+          "--upgrade pip\".")
+    sys.exit(1)
+
 
 # Solution from http://bit.ly/29Yl8VN
 def resolve_requires(requirements_file):
-    requirements = parse_requirements(os.path.join(
-        os.path.dirname(__file__), requirements_file), session=False)
-    return [str(ir.req) for ir in requirements]
+    try:
+        requirements = parse_requirements("./%s" % requirements_file,
+                                          session=False)
+        return [str(ir.req) for ir in requirements]
+    except AttributeError:
+        # for pip >= 20.1.x
+        # Need to run again as the first run was ruined by the exception
+        requirements = parse_requirements("./%s" % requirements_file,
+                                          session=False)
+        # pr stands for parsed_requirement
+        return [str(pr.requirement) for pr in requirements]
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-# Solution from: http://bit.ly/2mig8RT
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # We still running: python setup.py sdist upload --repository=testpypi
 # Twine isn't handling long_descriptions as per:
@@ -42,28 +52,32 @@ def read(fname):
 setup(
     name="Nutrac",
     version="0.1",
-    description="Intrac is a trac multi-project and login manager.",
-    long_description=read('README.rst'),
-    license="Apache License V2.0",
-    author="Flavio Garcia",
-    author_email="piraz@candango.org",
-    maintainer="Flavio Garcia",
-    maintainer_email="piraz@candango.org",
+    description="Nutrac is a trac multi-project and login manager.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=nutrac.__licence__,
+    author=nutrac.get_author(),
+    author_email=nutrac.get_author_email(),
+    maintainer=nutrac.get_author(),
+    maintainer_email=nutrac.get_author_email(),
     install_requires=resolve_requires("requirements.txt"),
+    python_requires=">= 3.8",
     url="https://github.com/candango/nutrac",
     packages=find_packages(),
     package_dir={'nutrac': "nutrac"},
     include_package_data=True,
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 1 - Planning",
         "Environment :: Console",
         "Environment :: Web Environment",
+        "Framework :: Trac",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
